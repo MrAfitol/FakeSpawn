e7271947703840b2f378f9d1e7682181087935b7
MrAfitol <76150070+MrAfitol@users.noreply.github.com>
2021-04-28 10:26:44 +0300

    Add files via upload

---
+++ FakeSpawn-Exiled/FakeSpawn/FakeSpawn.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API.Features;
+using System;
+
+namespace FakeSpawn
+{
+    public class FakeSpawn : Plugin<Config>
+    {
+        public override string Name => "FakeSpawn";
+
+        public override string Author => "MrAfitol";
+
+        public override Version Version => new Version(1, 0, 0);
+
+        public override Version RequiredExiledVersion => new Version(2, 10, 0);
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+
+    }
+}
